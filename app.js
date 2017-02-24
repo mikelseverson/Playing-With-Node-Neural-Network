1b47222c606bb77b32a0500a3b820c9ae42eec79
Mikel Severson <mkseve@gmail.com>
2017-02-23 19:58:58 -0600

    built server, add XOR neural network

--- app.js
+++ app.js
@@ -2,23 +2,57 @@ const express    = require('express'),
       synaptic   = require('synaptic'),
       path       = require('path'),
       bodyParser = require('body-parser'),
-      port       = 3000;
+      port       = 3000,
+      app        = express();
 
-const app = express();
+var inputLayer = new synaptic.Layer(2);
+var hiddenLayer = new synaptic.Layer(3);
+var outputLayer = new synaptic.Layer(1);
+inputLayer.project(hiddenLayer);
+hiddenLayer.project(outputLayer);
+
+/* Server */
+// Middlewear
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.listen(port, () => {
-  console.info('app is listening on port: ' + port);
+// Endpoints
+app.get('/', (req, res) => { res.sendFile(path.join(__dirname, './index.html')); });
+app.post('/train-network', (req, res) => {  res.send('Not Implemented');  });
+app.post('/input', (req, res) => {  res.send(myNetwork.activate(req.body.data)); });
+app.get('/*', (req, res) => { res.send('404'); })
+// Listen
+app.listen(port, () => { console.info('app is listening on port: ' + port); });
+/* End Server */
+
+
+// Initialize Nerwork
+var myNetwork = new synaptic.Network({
+    input: inputLayer,
+    hidden: [hiddenLayer],
+    output: outputLayer
 });
 
-app.post('/train-model', (req, res) => {
-  res.send('trained');
-});
+// Train the network
+var learningRate = .3;
+for (var i = 0; i < 20000; i++)
+{
+    //Training XOR Neural Network
+    // 0,0 => 0
+    myNetwork.activate([0,0]);
+    myNetwork.propagate(learningRate, [0]);
+    // 0,1 => 1
+    myNetwork.activate([0,1]);
+    myNetwork.propagate(learningRate, [1]);
+    // 1,0 => 1
+    myNetwork.activate([1,0]);
+    myNetwork.propagate(learningRate, [1]);
+    // 1,1 => 0
+    myNetwork.activate([1,1]);
+    myNetwork.propagate(learningRate, [0]);
+}
 
-app.post('/input', (req, res) => {
-  res.send('data');
-});
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './index.html'));
-});
+// Quick console.log test
+console.log(myNetwork.activate([0,0])); // [ 0.00006256959329031066 ]
+console.log(myNetwork.activate([0,1])); // [ 0.9995991812402573 ]
+console.log(myNetwork.activate([1,0])); // [ 0.9995991812402573 ]
+console.log(myNetwork.activate([1,1])); // [ 0.9995991812402573 ]
